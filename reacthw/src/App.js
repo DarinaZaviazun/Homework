915a2203f2ac4cd1db155e8d0fdb8a808745ff86
darinka <darinka.atrahova@gmail.com>
2020-12-11 18:03:22 +0200

    react practice3

--- reacthw/src/App.js
+++ reacthw/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import "./App.css"
+import {BrowserRouter as Router, Switch, Route, Link, withRouter} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Router>
+            <div>
+                <div className={"header"}>
+                    <Link id="home">Header</Link>
+                    <Link id="user" to={"/users"}>User</Link>
+                </div>
+                <div className={'appRoute'}>
+                    <Switch>
+                        <Route path={"/users"}>
+                            <AllUsers/>
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
