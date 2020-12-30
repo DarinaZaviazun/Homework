b2189d1970881e949ea450255f78d86b201a8965
DarinaZaviazun <72852127+DarinaZaviazun@users.noreply.github.com>
2020-12-30 13:57:01 +0200

    Delete index.js

--- react-redux-classwork29122020/src/reducer/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from "redux";
-import list from "./listWords"
-
-export const reducer = combineReducers({list});
-export const addValue = (payload) => ({type: "ADD_TO_LIST", payload})
-export const deleteValue = (payload) => ({type: "DELETE_FROM_LIST", payload})
