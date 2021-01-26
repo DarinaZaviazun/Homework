a77c9255a91215dddaecb5ae28dd47347be6603f
darinka <darinka.atrahova@gmail.com>
2021-01-26 13:33:31 +0200

    angular hw3

--- angular/src/app/car-info/car-info.component.ts
+++ angular/src/app/car-info/car-info.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {cars} from '../../data/database';
+import {cars} from '../data/database';
 import {ICar} from '../interfaces/carInterface';
 
 @Component({
@@ -11,13 +11,13 @@ import {ICar} from '../interfaces/carInterface';
 export class CarInfoComponent implements OnInit {
   cars = cars;
   id: number;
-  carCh: any;
+  carCh: ICar[];
   constructor(private activatedRoute: ActivatedRoute) {
   }
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(value => {
       this.id = value.id;
-      this.carCh = cars.filter(value1 => value1.id == this.id);
+      this.carCh = cars.filter(value1 => value1.id === +this.id);
     });
 
   }
--- angular/src/app/cars/cars.component.ts
+++ angular/src/app/cars/cars.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {cars} from '../../data/database';
+import {cars} from '../data/database';
 
 @Component({
   selector: 'app-cars',
