6c373b22820dca50754997ab2e82c1dbf2e302bd
darinka <darinka.atrahova@gmail.com>
2021-02-05 16:25:32 +0200

    angular class03022021 работа с локальным сервером

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { CarsComponent } from './components/main/cars/cars.component';
+import { CreateComponent } from './components/main/create/create.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
+    CarsComponent,
+    CreateComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
