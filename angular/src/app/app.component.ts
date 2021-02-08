b8b9137c3476006709b583bafd9ce13387dd98b3
darinka <darinka.atrahova@gmail.com>
2021-02-08 12:08:15 +0200

    angular homework8

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, DoCheck, ElementRef, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {DataService} from './services';
+import {AComponent} from './components/a/a.component';
+import {BComponent} from './components/b/b.component';
 
 
 @Component({
@@ -8,10 +10,37 @@ import {DataService} from './services';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnChanges, OnInit, DoCheck, OnDestroy, AfterViewInit{
+  @ViewChild(AComponent)
+  a: AComponent;
+  @ViewChild(BComponent)
+  b: BComponent;
+  @ViewChild('xxx')
+  xxx: ElementRef;
   constructor(public dataService: DataService) {
   }
   increment(): void{
     this.dataService.changeCounter();
+  }
+  ngOnInit(): void {
+    console.log('init');
+  }
+
+  ngDoCheck(): void {
+    console.log('docheck');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
+
+  ngOnDestroy(): void {
+    console.log('destroy');
+  }
+  ngAfterViewInit(): void{
+    console.log(this.a);
+    console.log(this.b);
+    this.xxx.nativeElement.innerText = 'tririririririririri';
+    console.log(this.xxx.nativeElement.innerText);
   }
 }
