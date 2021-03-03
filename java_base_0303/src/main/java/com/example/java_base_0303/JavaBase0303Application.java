bae6b44f728f35a7923c0e240be1e3d2e2e50d7b
darinka <darinka.atrahova@gmail.com>
2021-03-03 16:28:12 +0200

    java maven base firs empty project

---
+++ java_base_0303/src/main/java/com/example/java_base_0303/JavaBase0303Application.java
@@ -0,0 +1,13 @@
+package com.example.java_base_0303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBase0303Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBase0303Application.class, args);
+    }
+
+}
---
+++ java_base_0303/src/test/java/com/example/java_base_0303/JavaBase0303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_base_0303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBase0303ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
