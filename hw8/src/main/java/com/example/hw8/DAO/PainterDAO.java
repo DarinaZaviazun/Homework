75de61f53f7dfd30c65e073c9e9ee726214a4bd5
Daryna Zaviazun <darinka.atrahova@gmail.com>
2021-03-16 10:24:32 +0200

    homework8 11032021 Connections manytomany

--- hw8/src/main/java/com/example/hw8/DAO/PainterDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.hw8.DAO;
-
-import com.example.hw8.models.Painter;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@EnableJpaRepositories
-public interface PainterDAO extends JpaRepository<Painter, Integer> {
-}
